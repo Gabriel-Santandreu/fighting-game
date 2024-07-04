e74728f1b4af06cc64834b335d3bd08e0eed9cbc
Gabriel <gsantandreusantonja@gmail.com>
2024-07-04 12:19:24 +0200

    Funcionalidad mecánica completa - pendiente splash arts

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Sprite {
         };
         this.color = color;
         this.isAttacking;
+        this.health = 100;
     }
 
     draw() {
@@ -129,6 +130,35 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     );
 }
 
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+
+    document.querySelector("#displayText").style.display = "flex";
+    if (player.health === enemy.health) {
+        document.querySelector("#displayText").innerHTML = "Tie";
+    } else if (player.health > enemy.health) {
+        document.querySelector("#displayText").innerHTML = "Player 1 wins";
+    } else if (enemy.health > player.health) {
+        document.querySelector("#displayText").innerHTML = "Player 2 wins";
+    }
+}
+
+let timer = 60;
+
+let timerId;
+
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000);
+        timer--;
+        document.querySelector("#timer").innerHTML = timer;
+    }
+
+    if (timer === 0) {
+        determineWinner({ player, enemy });
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = "black";
@@ -163,7 +193,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false;
-        console.log("player on");
+        enemy.health -= 5;
+        document.querySelector("#enemyHealth").style.width = enemy.health + "%";
     }
 
     if (
@@ -174,7 +205,14 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false;
-        console.log("enemy on");
+        player.health -= 5;
+        document.querySelector("#playerHealth").style.width =
+            player.health + "%";
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId });
     }
 }
 
@@ -241,3 +279,5 @@ window.addEventListener("keyup", (event) => {
 console.log(player);
 
 animate();
+
+decreaseTimer();
