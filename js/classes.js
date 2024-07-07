00218a6d6cc98437de4edee9e087584a4df92226
Gabriel <gsantandreusantonja@gmail.com>
2024-07-07 13:17:14 +0200

    Hecha acción de atacar - pendiente sprite de enemigo

--- js/classes.js
+++ js/classes.js
@@ -121,13 +121,14 @@ class Fighter extends Sprite {
             canvas.height - 96
         ) {
             this.velocity.y = 0;
-            this.position.y = 330
+            this.position.y = 330;
         } else {
             this.velocity.y += gravity;
         }
     }
 
     attack() {
+        this.switchSprite("attack1");
         this.isAttacking = true;
         setTimeout(() => {
             this.isAttacking = false;
@@ -135,6 +136,12 @@ class Fighter extends Sprite {
     }
 
     switchSprite(sprite) {
+        if (
+            this.image === this.sprites.attack1.image &&
+            this.framesCurrent < this.sprites.attack1.framesMax - 1
+        )
+            return;
+
         switch (sprite) {
             case "idle":
                 if (this.image !== this.sprites.idle.image) {
@@ -162,6 +169,14 @@ class Fighter extends Sprite {
                 if (this.image !== this.sprites.fall.image) {
                     this.image = this.sprites.fall.image;
                     this.framesMax = this.sprites.fall.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+
+            case "attack1":
+                if (this.image !== this.sprites.attack1.image) {
+                    this.image = this.sprites.attack1.image;
+                    this.framesMax = this.sprites.attack1.framesMax;
                     this.framesCurrent = 0;
                 }
                 break;
