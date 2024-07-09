9be536f73a49703175521d72e9383b267c5d8e54
Gabriel <gsantandreusantonja@gmail.com>
2024-07-09 12:37:08 +0200

    Animación de muerte completa - pendiente interfaz

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ const player = new Fighter({
         takeHit: {
             imageSrc: "./img/Player1/TakeHit_whiteSilhouette.png",
             framesMax: 4,
+        },
+        death: {
+            imageSrc: "./img/Player1/Death.png",
+            framesMax: 6,
         }
     },
     attackBox: {
@@ -128,6 +132,10 @@ const enemy = new Fighter({
         takeHit: {
             imageSrc: "./img/Player2/Take_hit.png",
             framesMax: 3,
+        },
+        death: {
+            imageSrc: "./img/Player2/Death.png",
+            framesMax: 7,
         }
     },
     attackBox: {
@@ -250,42 +258,49 @@ function animate() {
 }
 
 window.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "d":
-            keys.d.pressed = true;
-            player.lastKey = "d";
-            break;
+    if (!player.dead) {
+        switch (event.key) {
+            case "d":
+                keys.d.pressed = true;
+                player.lastKey = "d";
+                break;
+    
+            case "a":
+                keys.a.pressed = true;
+                player.lastKey = "a";
+                break;
+    
+            case "w":
+                player.velocity.y = -20;
+                break;
+    
+            case " ":
+                player.attack();
+                break;
+        }
+    }
 
-        case "a":
-            keys.a.pressed = true;
-            player.lastKey = "a";
-            break;
+    if (!enemy.dead) {
+        switch(event.key) {
+            case "ArrowRight":
+                keys.ArrowRight.pressed = true;
+                enemy.lastKey = "ArrowRight";
+                break;
+    
+            case "ArrowLeft":
+                keys.ArrowLeft.pressed = true;
+                enemy.lastKey = "ArrowLeft";
+                break;
+    
+            case "ArrowUp":
+                enemy.velocity.y = -20;
+                break;
+    
+            case "Enter":
+                enemy.attack();
+                break;
+        }
 
-        case "w":
-            player.velocity.y = -20;
-            break;
-
-        case " ":
-            player.attack();
-            break;
-
-        case "ArrowRight":
-            keys.ArrowRight.pressed = true;
-            enemy.lastKey = "ArrowRight";
-            break;
-
-        case "ArrowLeft":
-            keys.ArrowLeft.pressed = true;
-            enemy.lastKey = "ArrowLeft";
-            break;
-
-        case "ArrowUp":
-            enemy.velocity.y = -20;
-            break;
-
-        case "Enter":
-            enemy.attack();
-            break;
     }
 });
 
